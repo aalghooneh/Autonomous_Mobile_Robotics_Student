3a076e6e69eaae006175b75760cac341b0d61a40
Jerry <jerrylu984@gmail.com>
2023-10-14 15:15:17 -0400

    prelab work sesh1

--- localization.py
+++ localization.py
@@ -2,7 +2,7 @@ from utilities import Logger, euler_from_quaternion
 from rclpy.time import Time
 from rclpy.node import Node
 
-from rclpy.qos import QoSProfile
+from rclpy.qos import QoSProfile, ReliabilityPolicy, DurabilityPolicy
 from nav_msgs.msg import Odometry as odom
 
 from rclpy import init, spin
@@ -16,19 +16,21 @@ class localization(Node):
         # TODO Part 3: Define the QoS profile variable based on whether you are using the simulation (Turtlebot 3 Burger) or the real robot (Turtlebot 4)
         # Remember to define your QoS profile based on the information available in "ros2 topic info /odom --verbose" as explained in Tutorial 3
 
-        odom_qos=...
+        odom_qos=QoSProfile(reliability=ReliabilityPolicy.BEST_EFFORT, durability=DurabilityPolicy.VOLATILE, depth=10)
         
         self.loc_logger=Logger("robot_pose.csv", ["x", "y", "theta", "stamp"])
         self.pose=None
         
         # TODO Part 3: subscribe to the position sensor topic (Odometry)
-        ...
+        self.create_subscription(odom, "/odom", self.odom_callback, odom_qos)
     
     
     def odom_callback(self, pose_msg):
         
         # TODO Part 3: Read x,y, theta, and record the stamp
-        self.pose=[ ... ]
+        yaw = euler_from_quaternion(pose_msg.pose.pose.orientation)
+
+        self.pose=[pose_msg.pose.pose.position.x, pose_msg.pose.pose.position.y, yaw, pose_msg.header.stamp]
         
         # Log the data
         self.loc_logger.log_values([self.pose[0], self.pose[1], self.pose[2], Time.from_msg(self.pose[3]).nanoseconds])
@@ -39,4 +41,15 @@ class localization(Node):
 # TODO Part 3
 # Here put a guard that makes the node run, ONLY when run as a main thread!
 
+if __name__=="__main__":
+
+    init()
+
+    localization_instance = localization()
+
+    try:
+        #Start running th eobject
+        spin(localization_instance)
+    except KeyboardInterrupt:
+        print("Exiting")
     
--- planner.py
+++ planner.py
@@ -17,13 +17,26 @@ class planner:
         elif self.type==TRAJECTORY_PLANNER:
             return self.trajectory_planner(goalPoint)
 
-
+    #is goal point relative or absolute??
     def point_planner(self, goalPoint):
+
         return goalPoint
 
     # TODO Part 6: Implement the trajectories here
     def trajectory_planner(self, goalPoint):
-        pass
         # the return should be a list of trajectory points: [ [x1,y1], ..., [xn,yn]]
-        # return 
+
+        num_points = 10
+        x_inc = 0.1
+        trajectory_points = []
+
+        #starting x
+        x = 0
+
+        for i in range(num_points):
+            y = x*x
+            trajectory_points.append([x, y])
+            x += x_inc
+
+        return trajectory_points
 
